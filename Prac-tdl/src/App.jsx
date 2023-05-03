d4c0a337b1027807fd70af5122e979e1df4c6592
Teddy Pascual <teddylayson2000@gmail.com>
2023-05-03 18:52:18 +0800

    finished to do list in the app

--- Prac-tdl/src/App.jsx
+++ Prac-tdl/src/App.jsx
@@ -1,15 +1,68 @@
+import { useState } from "react";
+
 function App() {
+  const [newItem, setNewItem] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return {...todo, completed};
+        }
+        return todo;
+      }) 
+
+    })
+  }
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title: newItem, completed: false },
+      ];
+    });
+    setNewItem("");
+  }
+ 
   return (
     <>
-      <form className="new-item-form">
+      <form onSubmit={handleSubmit} className="new-item-form">
         <div>
           <label htmlFor="item">New Input</label>
-          <input type="text" id="item" />
+          <input
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+            type="text"
+            id="item"
+          />
         </div>
         <button className="btn">Add</button>
       </form>
       <h4>To Do List:</h4>
+      <ul>
+        {todos.map((todo) => {
+          return (
+            <>
+              <li key={todo.id}>
+                <label>
+                  <input type="checkbox" checked={todo.completed} onChange={e => toggleTodo(todo.id, e.target.checked)} />
+                  {todo.title}
+                </label>
+                <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+              </li>
+            </>
+          );
+        })}
+      </ul>
     </>
-  )
+  );
 }
 export default App;
